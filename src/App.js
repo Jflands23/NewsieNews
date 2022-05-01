738c455e3dc0c96de87aac1e78c6e4b3adbcc700
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2022-04-30 18:32:04 -0700

    test pipeline deploy2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          V2
+          V2 POOP IN POOP
         </p>
         <a
           className="App-link"
