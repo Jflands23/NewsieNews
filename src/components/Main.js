d36a5ea290530ab9fa90d1272b599d5ee9a27323
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2022-05-16 21:08:49 -0700

    revised functionality for feed component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,27 @@
 import { API, graphqlOperation } from "aws-amplify"
 import { listFeeds } from "../graphql/queries"
 import React, { useState, useEffect } from 'react';
-import { IconButton, Paper } from "@material-ui/core";
+import { IconButton, Paper,List,ListItem,ListItemText } from "@material-ui/core";
+import { syncClasses } from "@aws-amplify/datastore/lib-esm/datastore/datastore";
+import { CallMissedSharp } from "@material-ui/icons";
+import {makeStyles} from "@material-ui/core"
 
+const useStyles = makeStyles({
+    active:{
+        background:'#C8C8C8',
+    }
+})
+
+let activeFeed;
+
+function feedClick(feed){
+    activeFeed = feed;
+    console.log("click feed "+ feed)
+}
 
 const Main = () => {
     const [feeds, setFeeds] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
         fetchFeeds()
@@ -24,24 +40,25 @@ const Main = () => {
     
     return (
     <div className = "BigDiv">
-        <div className = "feedList">
+        <List className = "feedList">
             { feeds.map(feed => {
                 return (
                     <Paper  variant="outlined" elevation={2}>
-                        <div className = "feedCard">
-                        <IconButton aria-label="COCKA">
-                        
-                        </IconButton>
-                        <div>
-                            <div className="feedTitle">{feed.name}</div>
-                            <div className="feedDesc">{feed.description}</div>
-                        </div>
-                        </div>              
-
+                        <ListItem
+                         button
+                         onClick={() => feedClick(feed.name)}
+                         className = {feed.name == activeFeed ? classes.active:null}  
+                         key = {feed.id}
+                        >
+                        <ListItemText
+                             className="feedTitle"
+                             primary={feed.name} />
+                        </ListItem>
                     </Paper>
                 )
             })}
-        </div>
+       
+        </List>
         <div>
             <aside className = "innertube">
                 <h3>Left Column</h3>
